0074c85b11349738f9d458c86af4418fc396bf56
prianshimogha <moghapriyanshi@gmail.com>
2024-12-30 17:39:29 +0530

    updated about us page

--- src/components/about us/About.js
+++ src/components/about us/About.js
@@ -39,7 +39,7 @@ export default function About() {
               <div
                 className={style.more}
                 onClick={() => toggleSection(1)}
-                style={{ cursor: "pointer", color: "##000000" }}
+                style={{ cursor: "pointer", color: "#000000" }}
               >
                 {visibleSections[1] ? "View Less" : "View More"}
               </div>
@@ -79,7 +79,6 @@ export default function About() {
           </div>
         </div>
 
-        {/* Additional sections follow the same pattern */}
         {/* Section 3 */}
         <div className={style.parentLeft}>
           <div className={style.contentLeft}>
@@ -97,9 +96,120 @@ export default function About() {
               <div
                 className={style.more}
                 onClick={() => toggleSection(3)}
-                style={{ cursor: "pointer", color: "##000000" }}
+                style={{ cursor: "pointer", color: "#000000" }}
               >
                 {visibleSections[3] ? "View Less" : "View More"}
+              </div>
+            </div>
+          </div>
+          <div className={style.aboutImg}>
+            <img src={aboutus1} alt="img"></img>
+          </div>
+        </div>
+
+        {/* Section 4 */}
+        <div className={style.parentRight}>
+          <div className={style.aboutImg}>
+            <img src={aboutus2} alt="img"></img>
+          </div>
+          <div className={style.contentRight}>
+            <div className={style.head}>4. Moderators</div>
+            <div className={style.mainContent}>
+              Moderators: Dr. Nidhan Singh, Sh. J.M. Garg, and other dedicated
+              contributors oversee discussions and ensure accuracy.
+              {visibleSections[4] && (
+                <span>
+                  {" "}
+                  Additional details about moderators can go here.
+                </span>
+              )}
+              <div
+                className={style.more}
+                onClick={() => toggleSection(4)}
+                style={{ cursor: "pointer", color: "#000000" }}
+              >
+                {visibleSections[4] ? "View Less" : "View More"}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Section 5 */}
+        <div className={style.parentLeft}>
+          <div className={style.contentLeft}>
+            <div className={style.head}>5. Major Contributors</div>
+            <div className={style.mainContent}>
+              Major Contributors: Sh. Saroj Kumar Kasaju (Flora Documentation of
+              Nepal), Dr. Pankaj Kumar (Orchidaceae), and many others play
+              significant roles.
+              {visibleSections[5] && (
+                <span>
+                  {" "}
+                  Additional details about major contributors can go here.
+                </span>
+              )}
+              <div
+                className={style.more}
+                onClick={() => toggleSection(5)}
+                style={{ cursor: "pointer", color: "#000000" }}
+              >
+                {visibleSections[5] ? "View Less" : "View More"}
+              </div>
+            </div>
+          </div>
+          <div className={style.aboutImg}>
+            <img src={aboutus1} alt="img"></img>
+          </div>
+        </div>
+
+        {/* Section 6 */}
+        <div className={style.parentRight}>
+          <div className={style.aboutImg}>
+            <img src={aboutus2} alt="img"></img>
+          </div>
+          <div className={style.contentRight}>
+            <div className={style.head}>
+              6. Details of Some Important Contributors
+            </div>
+            <div className={style.mainContent}>
+              Learn about key contributors such as Dr. M. Sabu, Dr. Nidhan
+              Singh, and others who have provided invaluable contributions.
+              {visibleSections[6] && (
+                <span>
+                  {" "}
+                  Additional details about these contributors can go here.
+                </span>
+              )}
+              <div
+                className={style.more}
+                onClick={() => toggleSection(6)}
+                style={{ cursor: "pointer", color: "#000000" }}
+              >
+                {visibleSections[6] ? "View Less" : "View More"}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Section 7 */}
+        <div className={style.parentLeft}>
+          <div className={style.contentLeft}>
+            <div className={style.head}>7. Top 10 Contributors Over the Years</div>
+            <div className={style.mainContent}>
+              Recognizing the top 10 contributors for their dedication and
+              efforts in the field of flora documentation.
+              {visibleSections[7] && (
+                <span>
+                  {" "}
+                  Additional details about the top 10 contributors can go here.
+                </span>
+              )}
+              <div
+                className={style.more}
+                onClick={() => toggleSection(7)}
+                style={{ cursor: "pointer", color: "#000000" }}
+              >
+                {visibleSections[7] ? "View Less" : "View More"}
               </div>
             </div>
           </div>
